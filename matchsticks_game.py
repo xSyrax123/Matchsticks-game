067ef346c79b809f764b92e9311c6073329f20ec
xSyrax123 <99150043+xSyrax123@users.noreply.github.com>
2022-02-16 19:15:58 -0300

    Update matchsticks_game.py

--- matchsticks_game.py
+++ matchsticks_game.py
@@ -1,42 +1,50 @@
-class MatchsticksGame:
-    def __init__(self):
-        self.matchsticks = 23
-        self.player2 = False
+class Game:
+    def __init__(self, players=('Player 1', 'Player 2'), initial_matchsticks=23):
+        self.players = players
+        self.player = None
+        self.turn = 0
+        self.matchsticks = initial_matchsticks
 
-
-    def _is_valid_input(self, player):
-        return 1 <= player <= 3
-            
+    def _current_player(self):
+        return self.players[self.turn % 2]
 
     def _show_matchsticks(self):
         return '||||| '*(self.matchsticks//5) + '|'*(self.matchsticks%5)
 
+    def get_move(self, player):
+        while True:
+            try:
+                matchsticks_to_remove = int(input(f'{player} removes matchsticks: '))
 
-    def _game_finish(self):
+                if 1 <= matchsticks_to_remove <= 3:
+                    return matchsticks_to_remove
+
+                print('You can delete only between 1 and 3 matchsticks.')
+            except ValueError:
+                print('The value entered is invalid. You can only enter numeric values.')
+
+    def _play_turn(self):
+        self.player = self._current_player()
+        self.turn += 1
+
+    def _game_finished(self):
         return self.matchsticks <= 0
-        
 
     def play(self):
-        print('Game starts')
+        print('Game starts.')
         print(self._show_matchsticks())
 
-        while True:
-            player = int(input(f'The player {self.player2+1} remove: '))
-
-            if not self._is_valid_input(player):
-                print('You can delete only between 1 and 3 matchsticks.')
-                continue
-
-            self.matchsticks -= player
+        while self.matchsticks > 0:
+            self._play_turn()
+            matchsticks_to_remove = self.get_move(self.player)
+            self.matchsticks -= matchsticks_to_remove
             print(self._show_matchsticks())
 
-            if self._game_finish():
-                print(f'The player {self.player2+1} is eliminated.')
+            if self._game_finished():
+                print(f'{self.player} is eliminated.')
                 break
-
-            self.player2 = not self.player2
 
 
 if __name__ == '__main__':
-    game = MatchsticksGame()
+    game = Game()
     game.play()
